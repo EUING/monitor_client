ad12633bad9db70aec39e62e94d0a491e68a2315
EUING <coke1324@naver.com>
2021-06-21 14:51:08 +0900

    FolderWatcher 수정
    1. 이동 생성자 구현
    2. watch_folder set 함수 추가
    3. std::queue<std::pair<DWORD, std::wstring>> 포인터 멤버 변수 추가

--- folder_watcher.h
+++ folder_watcher.h
@@ -5,34 +5,38 @@
 
 #include <future>
 #include <optional>
+#include <queue>
+#include <utility>
 #include <string>
 
 namespace my_rest_client {
 	class FolderWatcher {
 	public:
-		FolderWatcher();
+		FolderWatcher(std::queue<std::pair<DWORD, std::wstring>>* change_info, const std::wstring& watch_folder = L"");
 
 		FolderWatcher(const FolderWatcher&) = delete;
 		FolderWatcher& operator=(const FolderWatcher&) = delete;
 
-		FolderWatcher(FolderWatcher&&) = default;
-		FolderWatcher& operator=(FolderWatcher&&) = default;
+		FolderWatcher(FolderWatcher&&);
+		FolderWatcher& operator=(FolderWatcher&&);
 
 		~FolderWatcher();
 
 		bool IsRunning() { return stop_watching_event_ != NULL; }
 		void StopWatching();
-		bool StartWatching(const std::wstring& watch_folder);
+		bool StartWatching();
 		std::wstring GetWatchFolder() { return watch_folder_; }
+		void SetWatchFolder(const std::wstring& watch_folder) { watch_folder_ = watch_folder; }
 
 	private:
-		std::optional<std::shared_ptr<void>> InitWatching(const std::wstring& watch_folder);
+		std::optional<std::shared_ptr<void>> InitWatching();
 		void WatchingDirectory(std::shared_ptr<void> folder_handle_ptr);
 		void CloseEvent();
 
-		std::shared_future<void> thread_future_;
+		std::future<void> thread_future_;
 		HANDLE stop_watching_event_;  // 감시 종료 이벤트
 		std::wstring watch_folder_;  // 감시하는 경로
+		std::queue<std::pair<DWORD, std::wstring>>* change_info_;
 	};
 
 }  // namespace my_rest_client
--- folder_wathcer.cpp
+++ folder_wathcer.cpp
@@ -4,17 +4,42 @@
 
 #include <future>
 #include <iostream>
+#include <queue>
+#include <utility>
 #include <string>
 #include <optional>
 #include <memory>
 
 namespace my_rest_client {
-	FolderWatcher::FolderWatcher() : thread_future_{}, stop_watching_event_(NULL), watch_folder_() {
+	FolderWatcher::FolderWatcher(std::queue<std::pair<DWORD, std::wstring>>* change_info, const std::wstring& watch_folder /*= L""*/) 
+		: thread_future_{}, stop_watching_event_(NULL), change_info_(change_info), watch_folder_(watch_folder) {
 	}
 
 	FolderWatcher::~FolderWatcher() {
 		StopWatching();
 		CloseEvent();
+	}
+
+	FolderWatcher::FolderWatcher(FolderWatcher&& rhs) {
+		rhs.StopWatching();
+		thread_future_ = std::move(rhs.thread_future_);
+		watch_folder_ = std::move(rhs.watch_folder_);
+		change_info_ = rhs.change_info_;
+		rhs.change_info_ = nullptr;
+		stop_watching_event_ = rhs.stop_watching_event_;
+		rhs.stop_watching_event_ = NULL;
+	}
+
+	FolderWatcher& FolderWatcher::operator=(FolderWatcher&& rhs) {
+		rhs.StopWatching();
+		thread_future_ = std::move(rhs.thread_future_);
+		watch_folder_ = std::move(rhs.watch_folder_);
+		change_info_ = rhs.change_info_;
+		rhs.change_info_ = nullptr;
+		stop_watching_event_ = rhs.stop_watching_event_;
+		rhs.stop_watching_event_ = NULL;
+
+		return *this;
 	}
 
 	void FolderWatcher::CloseEvent() {
@@ -28,21 +53,19 @@ namespace my_rest_client {
 		if (NULL != stop_watching_event_) {
 			SetEvent(stop_watching_event_);
 			thread_future_.wait();
-			watch_folder_.clear();
 		}
 	}
 
-	bool FolderWatcher::StartWatching(const std::wstring& watch_folder) {
+	bool FolderWatcher::StartWatching() {
 		if (IsRunning()) {
 			return false;
 		}
 
-		std::optional<std::shared_ptr<void>> result = InitWatching(watch_folder);
+		std::optional<std::shared_ptr<void>> result = InitWatching();
 		if (!result.has_value()) {
 			return false;
 		}
 
-		watch_folder_ = watch_folder;
 		auto folder_handle = result.value();
 
 		thread_future_ = std::async(std::launch::async, &FolderWatcher::WatchingDirectory, this, folder_handle);
@@ -50,7 +73,7 @@ namespace my_rest_client {
 		return true;
 	}	
 
-	std::optional<std::shared_ptr<void>> FolderWatcher::InitWatching(const std::wstring& watch_folder_) {
+	std::optional<std::shared_ptr<void>> FolderWatcher::InitWatching() {
 		DWORD attribute = GetFileAttributes(watch_folder_.c_str());
 		if (INVALID_FILE_ATTRIBUTES == attribute) {
 			std::wcerr << L"GetFileAttributes Fail!\n";
@@ -62,7 +85,7 @@ namespace my_rest_client {
 			return std::nullopt;  // 폴더가 아니면 감시하지 않음
 		}
 
-		HANDLE handle = CreateFile(
+		HANDLE folder_handle = CreateFile(
 			watch_folder_.c_str(),
 			FILE_LIST_DIRECTORY,
 			FILE_SHARE_READ | FILE_SHARE_WRITE,
@@ -71,7 +94,7 @@ namespace my_rest_client {
 			FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED,  // 비동기로 처리하기 위한 플래그
 			0);
 		
-		if (INVALID_HANDLE_VALUE == handle) {
+		if (INVALID_HANDLE_VALUE == folder_handle) {
 			std::wcerr << L"CreateFile Fail!\n";
 			return std::nullopt;
 		}
@@ -82,7 +105,7 @@ namespace my_rest_client {
 
 		};
 
-		std::shared_ptr<void> handle_ptr(handle, invalid_deleter);
+		std::shared_ptr<void> folder_handle_ptr(folder_handle, invalid_deleter);
 
 		stop_watching_event_ = CreateEvent(NULL, TRUE, FALSE, NULL);
 		if (NULL == stop_watching_event_) {
@@ -90,7 +113,7 @@ namespace my_rest_client {
 			return std::nullopt;
 		}
 
-		return handle_ptr;
+		return folder_handle_ptr;
 	}
 
 	void FolderWatcher::WatchingDirectory(std::shared_ptr<void> folder_handle_ptr) {
@@ -150,34 +173,26 @@ namespace my_rest_client {
 				do {
 					fni = reinterpret_cast<FILE_NOTIFY_INFORMATION*>(&buffer.get()[offset]);
 
-					switch (fni->Action) {
-					case FILE_ACTION_ADDED: {
-						std::wcout << L"FILE_ACTION_ADDED\n";
-						break;
+					std::wstring full_path;
+					if (FILE_ACTION_RENAMED_OLD_NAME == fni->Action) {
+						std::wstring old_name(fni->FileName, fni->FileNameLength / 2);
+						std::wstring old_full_path = watch_folder_ + L"\\" + old_name;
+
+						offset += fni->NextEntryOffset;
+						fni = reinterpret_cast<FILE_NOTIFY_INFORMATION*>(&buffer.get()[offset]);  // NEW_NAME을 알기 위해 offset을 증가
+
+						std::wstring new_name(fni->FileName, fni->FileNameLength / 2);
+						std::wstring new_full_path = watch_folder_ + L"\\" + new_name;
+						full_path = old_full_path + L';' + new_full_path;
 					}
-					case FILE_ACTION_REMOVED: {
-						std::wcout << L"FILE_ACTION_REMOVED\n";
-						break;
-					}
-					case FILE_ACTION_MODIFIED: {
-						std::wcout << L"FILE_ACTION_MODIFIED\n";
-						break;
-					}
-					case FILE_ACTION_RENAMED_OLD_NAME: {
-						std::wcout << L"FILE_ACTION_RENAMED_OLD_NAME\n";
-						break;
-					}
-					case FILE_ACTION_RENAMED_NEW_NAME: {
-						std::wcout << L"FILE_ACTION_RENAMED_NEW_NAME\n";
-						break;
-					}
-					default: {
-						break;
-					}
+					else {
+						std::wstring name(fni->FileName, fni->FileNameLength / 2);
+						full_path = watch_folder_ + L"\\" + name;
 					}
 
-					std::wstring name(fni->FileName, fni->FileNameLength / 2);
-					std::wcout << name << std::endl;
+					if (change_info_) {
+						change_info_->push({ fni->Action, full_path });
+					}
 
 					offset += fni->NextEntryOffset;
 				} while (fni->NextEntryOffset != 0);
