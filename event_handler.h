c60a063e3dbbb33c545cdffe9453cc29ac875c0b
EUING <coke1324@naver.com>
2021-07-05 23:32:08 +0900

    event_handler 클래스 추가

---
+++ event_handler.h
@@ -0,0 +1,33 @@
+#ifndef MONITOR_CLIENT_EVENT_HANDLER_H_
+#define MONITOR_CLIENT_EVENT_HANDLER_H_
+
+#include <Windows.h>
+#include <stdint.h>
+
+#include <string>
+
+#include "notify_queue.h"
+
+namespace monitor_client {
+	class EventHandler {
+	public:
+		EventHandler(std::shared_ptr<NotifyQueue> notify_queue);
+
+		EventHandler(const EventHandler&) = default;
+		EventHandler& operator=(const EventHandler&) = default;
+
+		EventHandler(EventHandler&&) = default;
+		EventHandler& operator=(EventHandler&&) = default;
+
+		~EventHandler() = default;
+
+		void PushEvent(const uint8_t* buffer);
+
+	private:
+		void PushAddEvent(const std::wstring& relative_path);
+
+		std::shared_ptr<NotifyQueue> notify_queue_;
+	};
+
+}  // namespace monitor_client
+#endif
