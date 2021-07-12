553d0dc293b659f4ea214f11a9d5983be8b12352
EUING <coke1324@naver.com>
2021-07-12 17:57:13 +0900

    EventQueue 클래스 추가

---
+++ event_queue.h
@@ -0,0 +1,37 @@
+#ifndef MONITOR_CLIENT_EVENT_QUEUE_H_
+#define MONITOR_CLIENT_EVENT_QUEUE_H_
+
+#include <memory>
+#include <optional>
+#include <mutex>
+#include <queue>
+#include <string>
+
+#include "base_event.h"
+
+namespace monitor_client {
+	class EventQueue {
+	public:
+		EventQueue();
+
+		EventQueue(const EventQueue&) = delete;
+		EventQueue& operator=(const EventQueue&) = delete;
+
+		EventQueue(EventQueue&&) = delete;
+		EventQueue& operator=(EventQueue&&) = delete;
+
+		~EventQueue();
+
+		void Push(std::unique_ptr<monitor_client::BaseEvent>&& event);
+		void Pop();
+		const std::unique_ptr<monitor_client::BaseEvent>& Front();
+		void Break();
+
+	private:
+		std::queue<std::unique_ptr<monitor_client::BaseEvent>> event_queue_;
+		std::mutex notify_queue_m_;
+		std::condition_variable notify_queue_cv_;
+		bool break_;  // Pop 함수 종료를 위한 변수
+	};
+}  // namespace monitor_client
+#endif
