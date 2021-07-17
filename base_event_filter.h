48f224853fcc14abb8dc7df45bfdb1b689e02654
EUING <coke1324@naver.com>
2021-07-17 20:29:50 +0900

    EventFilter 클래스 추가

---
+++ base_event_filter.h
@@ -0,0 +1,31 @@
+#ifndef MONITOR_CLIENT_BASE_EVENT_FILTER_H_
+#define MONITOR_CLIENT_BASE_EVENT_FILTER_H_
+
+#include <memory>
+#include <string>
+
+#include "common_struct.h"
+#include "event_queue.h"
+
+namespace monitor_client {
+	class BaseEventFilter {
+	public:
+		BaseEventFilter() = default;
+
+		BaseEventFilter(const BaseEventFilter&) = default;
+		BaseEventFilter& operator=(const BaseEventFilter&) = default;
+
+		BaseEventFilter(BaseEventFilter&&) = default;
+		BaseEventFilter& operator=(BaseEventFilter&&) = default;
+
+		virtual ~BaseEventFilter() = default;
+
+		virtual void UploadFilter(std::shared_ptr<EventQueue> event_queue, const common_utility::ItemInfo& item_info) const = 0;
+		virtual void DownloadFilter(std::shared_ptr<EventQueue> event_queue, const std::wstring& relative_path) const = 0;
+		virtual void RenameFilter(std::shared_ptr<EventQueue> event_queue, const common_utility::ChangeNameInfo& item_info) const = 0;
+		virtual void RemoveFilter(std::shared_ptr<EventQueue> event_queue, const std::wstring& relative_path) const = 0;
+		virtual void LocalRemoveFilter(std::shared_ptr<EventQueue> event_queue, const std::wstring& relative_path) const = 0;
+		virtual void ConflictFilter(std::shared_ptr<EventQueue> event_queue, const std::wstring& relative_path) const = 0;
+	};
+}  // namespace monitor_client
+#endif
---
+++ event_filter.h
@@ -0,0 +1,36 @@
+#ifndef MONITOR_CLIENT_EVENT_FILTER_H_
+#define MONITOR_CLIENT_EVENT_FILTER_H_
+
+#include <memory>
+#include <string>
+
+#include "common_struct.h"
+#include "base_event_filter.h"
+#include "event_queue.h"
+#include "local_db.h"
+
+namespace monitor_client {
+	class EventFilter : public BaseEventFilter {
+	public:
+		EventFilter(std::unique_ptr<ItemDao>&& item_dao) : local_db_(std::move(item_dao)), BaseEventFilter() {};
+
+		EventFilter(const EventFilter&) = default;
+		EventFilter& operator=(const EventFilter&) = default;
+
+		EventFilter(EventFilter&&) = default;
+		EventFilter& operator=(EventFilter&&) = default;
+
+		~EventFilter() override = default;
+
+		void UploadFilter(std::shared_ptr<EventQueue> event_queue, const common_utility::ItemInfo& item_info) const override;
+		void DownloadFilter(std::shared_ptr<EventQueue> event_queue, const std::wstring& relative_path) const override;
+		void RenameFilter(std::shared_ptr<EventQueue> event_queue, const common_utility::ChangeNameInfo& change_name_info) const override;
+		void RemoveFilter(std::shared_ptr<EventQueue> event_queue, const std::wstring& relative_path) const override;
+		void LocalRemoveFilter(std::shared_ptr<EventQueue> event_queue, const std::wstring& relative_path) const override;
+		void ConflictFilter(std::shared_ptr<EventQueue> event_queue, const std::wstring& relative_path) const override;
+
+	private:
+		LocalDb local_db_;
+	};
+}  // namespace monitor_client
+#endif
